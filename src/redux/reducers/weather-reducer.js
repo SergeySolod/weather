165d01324f3524f2d3d60495fad9507d88c2c387
SergeySolod <48413787+SergeySolod@users.noreply.github.com>
2020-01-04 23:58:12 -0800

    State is ready

--- src/redux/reducers/weather-reducer.js
+++ src/redux/reducers/weather-reducer.js
@@ -4,6 +4,9 @@ const SET_CITY = 'WeatherAPI/weather-reducer/SET_CITY';
 const SET_COUNTRY = 'WeatherAPI/weather-reducer/SET_COUNTRY';
 const SET_DESCRIPTION = 'WeatherAPI/weather-reducer/SET_DESCRIPTION';
 const SET_MAIN = 'WeatherAPI/weather-reducer/SET_MAIN';
+const SET_CELSIUS = 'WeatherAPI/weather-reducer/SET_CELSIUS';
+const SET_TEMP_MAX = 'WeatherAPI/weather-reducer/SET_TEMP_MAX';
+const SET_TEMP_MIN = 'WeatherAPI/weather-reducer/SET_TEMP_MIN';
 
 let initialState = {
     city: undefined,
@@ -28,6 +31,15 @@ const weatherReducer = (state = initialState, action) => {
         case SET_MAIN: {
             return {...state, main: action.main}
         }
+        case SET_CELSIUS: {
+            return {...state, celsius: Math.floor(action.celsius - 273.15)}
+        }
+        case SET_TEMP_MAX: {
+            return {...state, temp_max: Math.floor(action.temp_max - 273.15)}
+        }
+        case SET_TEMP_MIN: {
+            return {...state, temp_min: Math.floor(action.temp_min - 273.15)}
+        }
         case SET_DESCRIPTION: {
             return {...state, description: action.description}
         }
@@ -36,10 +48,13 @@ const weatherReducer = (state = initialState, action) => {
     }
 }
 
-const setCity = (city) => ({type: SET_CITY, city})
-const setCountry = (country) => ({type: SET_COUNTRY, country})
-const setMain = (main) => ({type: SET_MAIN, main})
-const setDescription = (description) => ({type: SET_DESCRIPTION, description})
+const setCity = (city) => ({type: SET_CITY, city});
+const setCountry = (country) => ({type: SET_COUNTRY, country});
+const setMain = (main) => ({type: SET_MAIN, main});
+const setСelsius = (celsius) => ({type: SET_CELSIUS, celsius});
+const setTempMax = (temp_max) => ({type: SET_TEMP_MAX, temp_max});
+const setTempMin = (temp_min) => ({type: SET_TEMP_MIN, temp_min});
+const setDescription = (description) => ({type: SET_DESCRIPTION, description});
 
 export const fetchWeatherTC = (dispatch) => {
     return async (dispatch) => {
@@ -47,6 +62,9 @@ export const fetchWeatherTC = (dispatch) => {
         dispatch(setCity(data.name));
         dispatch(setCountry(data.sys.country));
         dispatch(setMain(data.weather[0].main));
+        dispatch(setСelsius(data.main.temp));
+        dispatch(setTempMax(data.main.temp_min));
+        dispatch(setTempMin(data.main.temp_max));
         dispatch(setDescription(data.weather[0].description));
     }
 }
